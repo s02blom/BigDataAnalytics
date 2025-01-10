228053be6c1689e4405a65dee51a3f62aa3c4d9a
Sofia BLom - Ender <sofia@familjenblom.se>
2025-01-10 10:20:44 +0100

    Added code for connecting to a database

--- Containers/cljMonitoring/db.py
+++ Containers/cljMonitoring/db.py
@@ -1,8 +1,17 @@
+import pymongo
+from pymongo import MongoClient
+from os import environ
 
-
-def get_connection():
-    pass
-
-def close_connection():
-    pass
-
+def get_connection() -> pymongo.synchronous.database.Database:
+    """Established a connection with the Mongo and connects to the database"""
+    try:
+        client = MongoClient(environ.get("DATABASE_HOST"), int(environ.get("DATABASE_PORT")))
+        db = client[environ.get("DATABASE_NAME")]
+        db.client.close()
+    except Exception as err:
+        print(err)
+    return db
+    
+def close_connection(db: pymongo.synchronous.database.Database):
+    """Closes the connection with the database"""
+    db.client.close()
