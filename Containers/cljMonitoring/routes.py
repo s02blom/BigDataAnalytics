02e5df1968f01a2af85f4449c7f3c4cf225e74b5
Sofia BLom - Ender <sofia@familjenblom.se>
2025-01-15 12:21:11 +0100

    Work in progress for charting data

--- Containers/cljMonitoring/routes.py
+++ Containers/cljMonitoring/routes.py
@@ -1,7 +1,16 @@
-from flask import Blueprint, render_template
-from . import db
+from flask import Blueprint, render_template, g
+import time
 blueprint = Blueprint("main", __name__)
 
 @blueprint.route("/", methods=["GET"])
 def home():
-    return render_template("home.html")
+    print("In / route")
+    print(g.start_time, flush=True)
+    fake_data = {"count": [0, 10, 20, 30, 40], "time": [time.localtime(10), time.localtime(20), time.localtime(30), time.localtime(40), time.localtime(50)]}
+    fake_data2 = []#{"count": [0, 10, 20, 30, 40], "time": [time.localtime(10), time.localtime(20), time.localtime(30), time.localtime(40), time.localtime(50)]}
+    
+    for i in [0, 10, 20, 30, 40]:
+        fake_data2.append({"y": i, "x": time.strftime('%H:%M:%S', time.localtime(i))})
+    # return """<h2> Hard coded </h2> <p>Here we listen to Pet Shop Boys</p>"""
+    # return render_template("home.html", files_lables=fake_data["count"], files_data=[time.strftime('%Y-%m-%dT%H:%M:%SZ', timestamp) for timestamp in fake_data["time"]] )
+    return render_template("home.html", files_data=fake_data2)
