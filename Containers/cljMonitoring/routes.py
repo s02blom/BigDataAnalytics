6890286c8405e8e73ca0744d92df6c14b465eb4d
Sofia BLom - Ender <sofia@familjenblom.se>
2025-01-17 11:22:24 +0100

    Clean up home and use data from timers

--- Containers/cljMonitoring/routes.py
+++ Containers/cljMonitoring/routes.py
@@ -4,13 +4,12 @@ blueprint = Blueprint("main", __name__)
 
 @blueprint.route("/", methods=["GET"])
 def home():
-    print("In / route")
-    print(g.start_time, flush=True)
-    fake_data = {"count": [0, 10, 20, 30, 40], "time": [time.localtime(10), time.localtime(20), time.localtime(30), time.localtime(40), time.localtime(50)]}
-    fake_data2 = []#{"count": [0, 10, 20, 30, 40], "time": [time.localtime(10), time.localtime(20), time.localtime(30), time.localtime(40), time.localtime(50)]}
-    
-    for i in [0, 10, 20, 30, 40]:
-        fake_data2.append({"y": i, "x": time.strftime('%H:%M:%S', time.localtime(i))})
-    # return """<h2> Hard coded </h2> <p>Here we listen to Pet Shop Boys</p>"""
-    # return render_template("home.html", files_lables=fake_data["count"], files_data=[time.strftime('%Y-%m-%dT%H:%M:%SZ', timestamp) for timestamp in fake_data["time"]] )
-    return render_template("home.html", files_data=fake_data2)
+    chart_data = {}    
+    for timer in g.timers.values():
+        timer_data = {}
+        timer_data["y"] = timer.data["count"]
+        timer_data["x"] = timer.data["iso_time"]
+        timer_data["chart_name"] = f"Chart for {timer.collection_name}"
+        chart_data[timer.collection_name] = timer_data
+
+    return render_template("home.html", files_data=chart_data["files"], zip=zip)
