6198544ca3a356779f45512bd23a79731530ee3d
Sofia BLom - Ender <sofia@familjenblom.se>
2025-01-20 15:16:05 +0100

    Make timer's stop by themselves and if count is 0 do not add

--- Containers/cljMonitoring/timers.py
+++ Containers/cljMonitoring/timers.py
@@ -45,6 +45,7 @@ class CollectionTimer(Timer):
         self.database = database
         self.interval = int(interval)
         self.data = {"count": [], "time": [], "iso_time": []}
+        self.self_stop = 0
         super().__init__()
 
     def timer(self) -> None:
@@ -52,13 +53,26 @@ class CollectionTimer(Timer):
         The function that is run once every interval. 
         Currently we only get the count for the collection and take the current time and save that in data
         """
+        if not (self.self_stop % 100):
+            if (self.check_if_self_stop()):
+                self.stop()
         collection = self.database[self.collection_name]
         count = collection.count_documents({})
-        timestamp = time.localtime()
-        self.data["count"].append(count)
-        self.data["time"].append(timestamp)
-        self.data["iso_time"].append(time.strftime('%H:%M:%S', timestamp))
-        # print(f"{self.collection_name}, {count}", flush=True)
+        if count != 0:
+            timestamp = time.localtime()
+            self.data["count"].append(count)
+            self.data["time"].append(timestamp)
+            self.data["iso_time"].append(time.strftime('%H:%M:%S', timestamp))
+            self.self_stop += 1
+
+    def check_if_self_stop(self, min_similarity_length: int) -> bool:
+        """
+        The function check if the last 5 recorded datapoints have the same count
+        
+        Parameters:
+        min_similarity_length (int): the number of items looked at to see if they are the same
+        """
+        return len(self.data["count"]) >= min_similarity_length and len(set(self.data["count"])) == 1
 
     def get_dataframe(self) -> pd.DataFrame:
         return pd.DataFrame(self.data, columns=["count", "time"])
